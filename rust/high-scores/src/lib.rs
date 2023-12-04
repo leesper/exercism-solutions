f82903fee9230cf57e5816ba2e1a01d2ea0e4c80
leesper <pascal7718@gmail.com>
2023-12-04 18:26:15 +0800

    finish high-scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.iter().last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut top_three = self.scores.to_vec();
+        top_three.sort();
+        top_three.reverse();
+        if top_three.len() > 3 {
+            top_three.truncate(3);
+        }
+        top_three
+    }
+}
